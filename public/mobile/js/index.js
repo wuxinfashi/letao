8c1abe019a7ceaf28a58b4b77221816e232b5b36
hrj <704164588@qq.com>
2018-05-21 22:55:48 +0800

    完成首页

--- public/mobile/js/index.js
+++ public/mobile/js/index.js
@@ -3,4 +3,10 @@ $(function () {
         deceleration:0.005,//减速系数,系数越大,滚动速度越慢,滚动值越小
         indicators:false
     })
+    var gallery = mui('.mui-slider');
+    gallery.slider(
+        {
+            interval:3000//自动轮播间隔
+        }
+    )
 })
